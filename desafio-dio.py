b1cddd9b16cdbb1be8b216013f1f86acd7c5ac61
CesarFernandesGit <cesarfernandesjob@gmail.com>
2025-10-26 18:54:19 -0300

    feat: Adiciona função de saque

--- desafio-dio.py
+++ desafio-dio.py
@@ -15,18 +15,39 @@ LIMITE_SAQUES = 3
 
 opcao_menu = (input(menu))
 
-if opcao_menu == "d":
-    
-    def depositar(valor, /):
+def depositar(valor, /):
+    if opcao_menu == "d":
         valor = float(input("Informe o valor do depósito: "))
-        
         if valor > 0:        
-                global saldo
+                global saldo, extrato
                 saldo += valor
-                global extrato
-                extrato += f"Depósito: R$ {valor:.2f}\n"
+                extrato += f"Depósito: R$ {valor:.2f}\n"    
         else:
             print("Operação falhou! O valor informado é inválido.")
-
 depositar(0)
-    
+
+def sacar():
+        if opcao_menu == "s":
+            global saldo, numero_saques, LIMITE_SAQUES
+            valor = float(input("Informe o valor do saque: "))
+            excedeu_saldo = valor > saldo
+            excedeu_limite = valor > limite
+            excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+            if excedeu_saldo:
+                print("Operação falhou! Você não tem saldo suficiente.")
+
+            elif excedeu_limite:
+                print("Operação falhou! O valor do saque excede o limite.")
+
+            elif excedeu_saques:
+                print("Operação falhou! Número máximo de saques excedido.")
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                numero_saques += 1
+
+            else:
+                print("Operação falhou! O valor informado é inválido.") 
+sacar()
