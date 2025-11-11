472a9e4ce33df4a870c3d37be92491f75b2517fc
CesarFernandesGit <cesarfernandesjob@gmail.com>
2025-11-11 13:16:54 -0300

    refactor: Implementando iteradores nas funções de Transações, Saque e Depósito já existentes

--- desafio-dio.py
+++ desafio-dio.py
@@ -184,6 +184,42 @@ class Historico:
             if data_atual == data_transacao:
                 transacoes.append(transacao)
         return transacoes
+    
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)  
 
 def menu():
     menu = """
